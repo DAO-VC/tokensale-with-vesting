d6a7d3b209e9bc0d8f6ae1329b91cbece0a7634e
devmanai <ivan.kocheshev@gmail.com>
2023-03-29 17:06:31 +0200

    update deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,22 +6,42 @@ import "../src/Treasury.sol";
 import "../src/Market.sol";
 
 contract DeployScript is Script {
-    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address constant SharkToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    //uint256 constant tokenCap = 
-
+    address constant CurrencyToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant ProductToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    Treasury public treasury;
+    Market public market;
 
     function setUp() public {}
 
-    function run() public {
+    function deployMarketAndTreasury() public {
         string memory seedPhrase = vm.readFile(".secret");
         uint256 privateKey = vm.deriveKey(seedPhrase, 0);
         vm.startBroadcast(privateKey);
         // deploy tresary and market
-        Treasury tresary = new Treasury(SharkToken);
-        Market market = new Market(USDC, address(tresary));
+        treasury = new Treasury(ProductToken);
+        market = new Market(CurrencyToken, address(treasury));
 
         vm.stopBroadcast();
+    }
+
+    function setWhiteList() public {
+
+    }
+
+    function setTeamVestingCalendar() public {
+
+    }
+
+    function setPrivateRound() public {
+
+    }
+
+    function setPublicRound() public {
+
+    }
+
+    function setSeedRound() public {
+
     }
 }
 // https://ethereum-blockchain-developer.com/2022-06-nft-truffle-hardhat-foundry/16-foundry-deployment/
