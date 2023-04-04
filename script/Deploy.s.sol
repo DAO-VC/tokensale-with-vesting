c04c49325afa8d651da3fbc606352168eca2890d
devmanai <ivan.kocheshev@gmail.com>
2023-04-04 14:45:42 +0200

    set up first 5 rounds in deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -41,12 +41,18 @@ contract DeployScript is Script {
         
         market = new Market(CurrencyToken, address(treasury), CurrencyTreasury);
         treasury.transferOwnership(address(market));
-        deployMarket(marketMasterData);
+        //deployMarket(marketMasterData);
+        // tge, cliff, duration, slice, price
+        deployNewRound(3000, 12 weeks, 60 weeks, 4 weeks, 10); //Seed
+        deployNewRound(5000, 12 weeks, 60 weeks, 4 weeks, 12); //Private
+        deployNewRound(7000, 12 weeks, 60 weeks, 4 weeks, 14); //Strategic
+        deployNewRound(40000, 0 weeks, 24 weeks, 4 weeks, 20); //Public
+        deployNewRound(25000, 0 weeks, 32 weeks, 4 weeks, 17); //Whitelist
 
         vm.stopBroadcast();
     }
 
-    function deployMarket(Market.MarketInfo memory _data) public {
+    function deployRound(Market.MarketInfo memory _data) public {
                 market.deployMarket(    _data.price,
                                         _data.minOrderSize,
                                         _data.maxOrderSize,
@@ -59,6 +65,18 @@ contract DeployScript is Script {
                                         _data.permisionLess 
                                         );
     }
+
+    function deployNewRound(uint256 _tge, uint256 _cliff, uint256 _duration, uint256 _slice, uint256 _price) public {
+        marketMasterData.tgeRatio = _tge; 
+        marketMasterData.cliff = _cliff; 
+        marketMasterData.duration = _duration; 
+        marketMasterData.slicePeriod = _slice; 
+        marketMasterData.price = _price; 
+        deployRound(marketMasterData);
+
+    }
+
+
 
 /*
     function deployWhiteList() public {
