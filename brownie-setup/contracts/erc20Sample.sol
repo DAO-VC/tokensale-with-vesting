65bab907f31498f9031349d76b34638934548fd6
Stan <st.takt@gmail.com>
2023-04-20 18:24:38 +0300

    added erc20Sample

---
+++ brownie-setup/contracts/erc20Sample.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract erc20Sample is ERC20{
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(to!=address(0),"Zero address sent");
+        _mint(to, amount);
+    }
+
+    function decimals() public view virtual override(ERC20) returns (uint8) {
+        return 18;
+    }
+
+}
