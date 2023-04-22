ea85ee5fd32a9c4471275f5a81089075b46a2b07
egoreremin <suvenick2@gmail.com>
2023-04-22 09:48:11 +0300

    fixes

--- brownie-setup/scripts/deploy-all.py
+++ brownie-setup/scripts/deploy-all.py
@@ -17,10 +17,10 @@ def main():
     treasury = Treasury.deploy(shark_token.address, {'from': acct_adm})
     market = Market.deploy(usd_token.address, treasury.address, acct_adm, {'from': acct_adm})
     treasury.transferOwnership(market.address, {'from': acct_adm})
-    #deployNewRound(_tge=3000, _cliff=12 * week, _duration=60 * week, _slice=60 * 10, _price=10, market=market, acct_adm=acct_adm, isInternal=False) #seed
+    deployNewRound(_tge=3000, _cliff=12 * week, _duration=60 * week, _slice=60 * 10, _price=10, market=market, acct_adm=acct_adm, isInternal=False) #seed
     deployNewRound(0, 0, 60 * 5 * 20, 60 * 5, 12, market, acct_adm, isInternal=False)  # Private
-    # deployNewRound(7000, 12 * week, 60 * week, 60 * 10, 14, market, acct_adm, isInternal=False) #Strategic
-    # deployNewRound(40000, 0 * week, 24 * week, 60 * 10, 20, market, acct_adm, isInternal=False) #Public
+    deployNewRound(7000, 12 * week, 60 * week, 60 * 10, 14, market, acct_adm, isInternal=False) #Strategic
+    deployNewRound(40000, 0 * week, 24 * week, 60 * 10, 20, market, acct_adm, isInternal=False) #Public
     deployNewRound(25000, 0 * week, 60 * 5 * 20, 60 * 5, 17, market, acct_adm, permissionLess=False, isInternal=False) #Witelist
     deployNewRound(27000, 0 * week, 60 * 5 * 20, 60 * 5, 1e27, market, acct_adm, isInternal=False)  # Liquidity
     # deployNewRound(33000, 0 weeks, 8 weeks, 4 weeks, 1e27, market, acct_adm, isInternal=False); // Giveaways
@@ -35,15 +35,17 @@ def main():
     market.grantRole('07f9e8c07c8b73fe513688ff4c27d5672674617e83dca1f12879f7b9a12c25de', '0x5aCD656a61d4b2AAB249C3Fe3129E3867ab99283')
     market.grantRole('07f9e8c07c8b73fe513688ff4c27d5672674617e83dca1f12879f7b9a12c25de', '0xB09ecEe4335B97CbE76521c317081Bccf83d6190')
     market.grantRole('07f9e8c07c8b73fe513688ff4c27d5672674617e83dca1f12879f7b9a12c25de', '0x1892de64127590BF0a8a0B989ff342681286143B')
+    market.grantRole('07f9e8c07c8b73fe513688ff4c27d5672674617e83dca1f12879f7b9a12c25de', '0xB09ecEe4335B97CbE76521c317081Bccf83d6190')
     market.grantRole('af290d8680820aad922855f39b306097b20e28774d6c1ad35a20325630c3a02c', '0xfb592B5947798B1cfcDaD72619a8e6eE98924992')
     market.grantRole('af290d8680820aad922855f39b306097b20e28774d6c1ad35a20325630c3a02c', '0x5aCD656a61d4b2AAB249C3Fe3129E3867ab99283')
     market.grantRole('af290d8680820aad922855f39b306097b20e28774d6c1ad35a20325630c3a02c', '0xB09ecEe4335B97CbE76521c317081Bccf83d6190')
     market.grantRole('af290d8680820aad922855f39b306097b20e28774d6c1ad35a20325630c3a02c', '0x1892de64127590BF0a8a0B989ff342681286143B')
+    market.grantRole('af290d8680820aad922855f39b306097b20e28774d6c1ad35a20325630c3a02c', '0xB09ecEe4335B97CbE76521c317081Bccf83d6190')
     shark_token.mint(treasury.address, 1_000_000_000e18, {'from': acct_adm})
-    usd_token.mint('0x5aCD656a61d4b2AAB249C3Fe3129E3867ab99283', 1_000_000e18, {'from': acct_adm})
+    usd_token.mint('0xB09ecEe4335B97CbE76521c317081Bccf83d6190', 1_000_000e18, {'from': acct_adm})
 
 
-def deployNewRound(_tge, _cliff, _duration, _slice, _price, market, acct_adm, start=1681807247, minOrderSize=1e18, maxOrderSize=10_000e18, revocable=False, permissionLess=True, isInternal=True):
+def deployNewRound(_tge, _cliff, _duration, _slice, _price, market, acct_adm, start=1682010220, minOrderSize=1e18, maxOrderSize=10_000e18, revocable=False, permissionLess=True, isInternal=True):
     deployRound(_price, minOrderSize, maxOrderSize, _tge, start, _cliff, _duration, _slice, revocable, permissionLess, market, isInternal, acct_adm)
 
 
